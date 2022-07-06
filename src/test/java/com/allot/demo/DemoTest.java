337f96a135a42210846286aaf65863a81b0c2323
vafanasenka-frontlineed <vafanasenka@frontlineed.com>
2022-07-06 11:38:52 +0300

    fix me

--- src/test/java/com/allot/demo/DemoTest.java
+++ src/test/java/com/allot/demo/DemoTest.java
@@ -3,7 +3,6 @@ package com.allot.demo;
 import com.allot.cs.CSBaseTest;
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Description;
-import io.qameta.allure.Step;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -15,14 +14,9 @@ class DemoTest extends CSBaseTest {
     @Description("Demo test")
     @Test
     void demoTest(SoftAssertions soft) {
-        open();
-        Selenide.$(By.name("q")).shouldBe(visible).setValue("gegege");
-        soft.assertThat(Selenide.$(By.name("q")).val()).contains("gegege");
-    }
-
-    @Step("Demo test")
-    private void open() {
         Selenide.open("https://www.google.com/");
+        Selenide.$(By.name("q")).shouldNotBe(visible).setValue("gegege");
+        soft.assertThat(Selenide.$(By.name("q")).val()).contains("gegege");
     }
 
 }
